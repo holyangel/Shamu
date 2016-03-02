a4ab2f23506bc4833d762410546dcfc2ea4bbef9
Jonathan Wicks <jwicks@codeaurora.org>
2016-03-02 11:06:11 -0700

    msm: kgsl: Unbind the kgsl-event workqueue
    Allow the kgsl-event workqueue to run on any available
    core. The other workqueues in KGSL were already unbound.
    
    CRs-Fixed: 985082
    Change-Id: I7e843b57541b7ddcb53848078f73b05c88238711
    Signed-off-by: Jonathan Wicks <jwicks@codeaurora.org>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4450,7 +4450,6 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 				PM_QOS_CPU_DMA_LATENCY,
 				PM_QOS_DEFAULT_VALUE);
 
-
 	device->events_wq = alloc_workqueue("kgsl-events",
 		WQ_HIGHPRI | WQ_UNBOUND | WQ_MEM_RECLAIM, 0);
 
