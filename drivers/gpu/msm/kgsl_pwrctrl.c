3891ac7be491c4d7178966975c665a246fa72d3b
franciscofranco <franciscofranco.1990@gmail.com>
2016-04-12 00:53:47 +0000

    Fix the remaining HZ references to the old 100HZ value from https://android.googlesource.com/kernel/msm/+/725704a29ae813c20c6a98d01e4daa63a6468052
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	drivers/gpu/msm/kgsl_pwrctrl.c

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -1,3 +1,4 @@
+
 /* Copyright (c) 2010-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -500,8 +501,10 @@ static ssize_t kgsl_pwrctrl_idle_timer_show(struct device *dev,
 					char *buf)
 {
 	struct kgsl_device *device = kgsl_device_from_dev(dev);
+
 	if (device == NULL)
 		return 0;
+
 	/* Show the idle_timeout in msec */
 	return snprintf(buf, PAGE_SIZE, "%d\n",
 		jiffies_to_msecs(device->pwrctrl.interval_timeout));
