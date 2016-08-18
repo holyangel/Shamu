1c2bc47787f0957de5bf447fe2613b85536551cd
Francisco Franco <franciscofranco.1990@gmail.com>
2016-08-18 22:36:52 +0000

    cpufreq: force all cores to use the same governor
    If BCL is enabled, and under certain conditions cpu2 and cpu3
    are pegged to max freq when they come back online (if they've been
    offlined by BCL). If this happens during boot cpu2 and cpu3 lose
    their policy governor reference (I don't know why) and simply fall
    back to the default performance governor.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1997,6 +1997,7 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 				struct cpufreq_policy *new_policy)
 {
 	int ret = 0, failed = 1;
+	struct cpufreq_policy *cpu0_policy = NULL;
 
 	pr_debug("setting new policy for CPU %u: %u - %u kHz\n", new_policy->cpu,
 		new_policy->min, new_policy->max);
@@ -2038,6 +2039,9 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	policy->max = new_policy->max;
 	trace_cpu_frequency_limits(policy->max, policy->min, policy->cpu);
 
+	if (new_policy->cpu)
+		cpu0_policy = cpufreq_cpu_get(0);
+
 	pr_debug("new min and max freqs are %u - %u kHz\n",
 					policy->min, policy->max);
 
@@ -2062,7 +2066,10 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 			}
 
 			/* start new governor */
-			policy->governor = new_policy->governor;
+			if (new_policy->cpu && cpu0_policy)
+				policy->governor = cpu0_policy->governor;
+			else
+				policy->governor = new_policy->governor;
 			if (!__cpufreq_governor(policy, CPUFREQ_GOV_POLICY_INIT)) {
 				if (!__cpufreq_governor(policy, CPUFREQ_GOV_START)) {
 					failed = 0;
